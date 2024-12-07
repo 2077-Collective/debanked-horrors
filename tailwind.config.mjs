cc93a45bd99124568988dbaa40057cbf54babdbb
Lyn <0xlynett@proton.me>
2024-12-07 17:51:50 +0000

    fix font 2

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,12 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        diphylleia: "Diphylleia, sans-serif",
+        diphylleia: ["Diphylleia", ...defaultTheme.fontFamily.sans],
       },
     },
   },
